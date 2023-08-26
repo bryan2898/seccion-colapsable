df4c698e8653501b38076c461ea8b15782c49d12
bryan2898 <bssantana0@soy.sena.edu.co>
2023-08-26 17:31:46 -0500

    vs2

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+let colapso = document.getElementsByClassName("colapso");
+let i;
+
+for (i = 0; i < colapso.length; i++) {
+    colapso[i].addEventListener("click",function () {
+        this.classList.toggle("active");
+        let contenido =this.nextElementSibling;
+        if (contenido.style.display=== "block") {
+            contenido.style.display ="none";
+        } else {
+            contenido.style.display ="block";
+        }
+    });
+}
